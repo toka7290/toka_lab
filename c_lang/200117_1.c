993e1846cdc670730f344239ddb53f6f2464f90a
とかさん <46467578+toka7290@users.noreply.github.com>
2020-01-17 15:20:39 +0900

    15回

--- c_lang/200117_1.c
+++ c_lang/200117_1.c
@@ -5,20 +5,20 @@
 
 #define MAX 10
 
-typedef struct {
+typedef struct {//色管理用の構造体
   int red;
   int green;
   int blue;
 } Color;
 
-typedef struct {
+typedef struct {//画像管理用の構造体
   int width;
   int heght;
   Color **data;
 } Image;
 
 
-void fill_white_canvas(Image image){
+void fill_white_canvas(Image image){//キャンバスを白に
   int i,j;
   Color white = {255,255,255};
   //Color green = {128,255};
@@ -30,7 +30,7 @@ void fill_white_canvas(Image image){
   }
 }
 
-void save_file(Image image){
+void save_file(Image image){//保存
   FILE *fp;
   if ((fp = fopen("img_x4.ppm", "w")) != NULL){
     fputs("P3\n",fp);//P3モード
@@ -48,7 +48,7 @@ void save_file(Image image){
   }
 }
 
-Color **make_data(int width, int heght){
+Color **make_data(int width, int heght){//メモリ確保
     int i;
     Color **data;
     data = (Color **)malloc(sizeof(Color *) * width);
@@ -56,7 +56,7 @@ Color **make_data(int width, int heght){
     return data;
 }
 
-void draw_line(Image image, int x1, int y1, int x2, int y2, Color linecolor){
+void draw_line(Image image, int x1, int y1, int x2, int y2, Color linecolor){//線描画
   int i;
   int x = x1,y = y1;
 
@@ -77,7 +77,7 @@ void draw_line(Image image, int x1, int y1, int x2, int y2, Color linecolor){
       if(
         x >= image.width || x < 0 ||
         y >= image.heght || y < 0 )return;//範囲外で終了
-      image.data[y][x] = linecolor;
+      image.data[y][x] = linecolor;//着色
       x += sx;
       j += dy2;
       if (0 <= j) {
@@ -102,21 +102,21 @@ void draw_line(Image image, int x1, int y1, int x2, int y2, Color linecolor){
   }
 }
 
-void draw_rect(Image image, int x1, int y1, int x2, int y2, Color linecolor){
+void draw_rect(Image image, int x1, int y1, int x2, int y2, Color linecolor){//矩形
   draw_line(image,x1,y1,x1,y2,linecolor);
   draw_line(image,x1,y1,x2,y1,linecolor);
   draw_line(image,x2,y1,x2,y2,linecolor);
   draw_line(image,x1,y2,x2,y2,linecolor);
 }
 	
-void draw_quad(Image image, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, Color linecolor){
+void draw_quad(Image image, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, Color linecolor){//四角形
   draw_line(image,x1,y1,x2,y2,linecolor);
   draw_line(image,x2,y2,x3,y3,linecolor);
   draw_line(image,x3,y3,x4,y4,linecolor);
   draw_line(image,x4,y4,x1,y1,linecolor);
 }	
 
-void draw_circle(Image image, int cx, int cy,int extent, Color linecolor){
+void draw_circle(Image image, int cx, int cy,int extent, Color linecolor){//円描画
   double r;
   for(r=0.0;r<2*M_PI;r+=M_1_PI/10000.0){
     double y = sin(r)*extent;
@@ -125,7 +125,7 @@ void draw_circle(Image image, int cx, int cy,int extent, Color linecolor){
   }
 }
 
-void draw_sinewave(Image image, int x1, int y1, int x2, int y2, int extent,double freq,Color linecolor){
+void draw_sinewave(Image image, int x1, int y1, int x2, int y2, int extent,double freq,Color linecolor){//正弦
   int dx = abs(x2 - x1);
   int dy = abs(y2 - y1);
   int sx = (x1 < x2) ? 1 : -1;
@@ -140,7 +140,7 @@ void draw_sinewave(Image image, int x1, int y1, int x2, int y2, int extent,doubl
   }
 }
 
-void fill_bucket_s(Image image,int x,int y, Color color, Color targetcolor){
+void fill_bucket_s(Image image,int x,int y, Color color, Color targetcolor){//塗り潰し本体
   image.data[y][x]=color;
   if(image.width > x+1){
     if(image.data[y][x+1].red==targetcolor.red &&
@@ -172,7 +172,7 @@ void fill_bucket_s(Image image,int x,int y, Color color, Color targetcolor){
   }
 }
 
-void fill_bucket(Image image,int x,int y, Color color){
+void fill_bucket(Image image,int x,int y, Color color){//塗り潰し
   if(image.data[y][x].red==color.red &&
       image.data[y][x].green==color.green &&
       image.data[y][x].blue==color.blue )return;
@@ -180,7 +180,7 @@ void fill_bucket(Image image,int x,int y, Color color){
   fill_bucket_s(image,x,y,color,targetcolor);
 }
 
-void fill_rect(Image image, int x1, int y1, int x2, int y2, Color color){
+void fill_rect(Image image, int x1, int y1, int x2, int y2, Color color){//
   draw_line(image,x1,y1,x1,y2,color);
   draw_line(image,x1,y1,x2,y1,color);
   draw_line(image,x2,y1,x2,y2,color);
@@ -210,7 +210,7 @@ void fill_circle(Image image, int cx, int cy,int extent, Color color){
   fill_bucket(image,cx,cy, color);
 }
 
-void replace_color(Image image, Color replace_col, Color color){
+void replace_color(Image image, Color replace_col, Color color){//色置き換え
   int i,j;
   for(i=0;i<image.width;i++){
     for(j=0;j<image.heght;j++){
@@ -238,34 +238,38 @@ int main(void){
         blue = {0,128,255},
         yellow = {255,224,0},
         darkblue = {32,64,128};
-
+	//円描画
   draw_circle(ppm,200,200,172,black);
   fill_bucket(ppm,196,196,darkblue);
 
+	//赤の部分
   draw_sinewave(ppm,108,100,208,100,-16,M_PI,black);
   draw_sinewave(ppm,92,196,192,196,-16,M_PI,black);
   draw_line(ppm,108,100,92,196,black);
   draw_line(ppm,208,100,192,196,black);
   fill_bucket(ppm,140,140,red);
 
+	//緑の部分
   draw_sinewave(ppm,220,104,324,104,16,M_PI,black);
   draw_sinewave(ppm,204,200,308,200,16,M_PI,black);
   draw_line(ppm,220,104,204,200,black);
   draw_line(ppm,324,104,308,200,black);
   fill_bucket(ppm,240,140,green);
 
+	//青の部分
   draw_sinewave(ppm,90,208,190,208,-16,M_PI,black);
   draw_sinewave(ppm,74,304,174,304,-16,M_PI,black);
   draw_line(ppm,90,208,74,304,black);
   draw_line(ppm,190,208,174,304,black);
   fill_bucket(ppm,140,240,blue);
 
+	//黄色の部分
   draw_sinewave(ppm,202,212,306,212,16,M_PI,black);
   draw_sinewave(ppm,186,308,290,308,16,M_PI,black);
   draw_line(ppm,202,212,186,308,black);
   draw_line(ppm,306,212,290,308,black);
   fill_bucket(ppm,240,240,yellow);
 
-  save_file(ppm);
+  save_file(ppm);//保存
   return 0;
 }
